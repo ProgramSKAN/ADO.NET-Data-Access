60e28457e1f59d5f3250097e6e68cdc21555dbf6
SandeepKandagatla <kandagatlas@gmail.com>
2020-05-13 20:04:46 +0530

    DataView
    TODO

---
+++ ADO.NET/Settings/Connect and Submit Sql To Db/DataView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ADO.NET.Settings.Connect_and_Submit_Sql_To_Db
+{
+    public class DataView
+    {
+        //A major function of the DataView is to allow data binding on both Windows Forms and Web Forms.
+    }
+}
